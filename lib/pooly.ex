f98629e0780015b1bf57740345e5528b000699f9
Muwanguzi Peter <petermuwanguzi51@gmail.com>
2022-02-04 11:18:48 +0300

    cpt-version1

--- lib/pooly.ex
+++ lib/pooly.ex
@@ -1,18 +1,29 @@
 defmodule Pooly do
-  @moduledoc """
-  Documentation for `Pooly`.
-  """
+  use Application
 
-  @doc """
-  Hello world.
+  @spec start(any, any) :: {:error, any} | {:ok, pid}
+  def start(_type, _args) do
+    pool_config = [mfa: {SampleWorker, :start_link, []}, size: 5]
+    start_pool(pool_config)
+  end
 
-  ## Examples
+  @spec start_pool(any) :: :ignore | {:error, any} | {:ok, pid}
+  def start_pool(pool_config) do
+    Pooly.Supervisor.start_link(pool_config)
+  end
 
-      iex> Pooly.hello()
-      :world
+  @spec checkout :: any
+  def checkout do
+    Pooly.Server.checkout
+  end
 
-  """
-  def hello do
-    :world
+  @spec checkin(any) :: :ok
+  def checkin(worker_pid) do
+    Pooly.Server.checkin(worker_pid)
+  end
+
+  @spec status :: any
+  def status do
+    Pooly.Server.status
   end
 end
